205381501165c7441c688a145b53da02b3bdfefe
davidhome <ducanhnguyen145@gmail.com>
2024-05-22 13:28:39 +0300

    HomePageUi

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,23 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 function Home() {
+    const loggedIn = {firstname: 'david'}
     return ( 
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                    type="greeting"
+                    title="welcome"
+                    user={loggedIn?.firstname || 'Guest'}
+                    subtext="Access and manage your account and transactions efficiently"/>
+                    <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250}/>
+                </header>
+            </div>
+        </section>
      );
 }
 
